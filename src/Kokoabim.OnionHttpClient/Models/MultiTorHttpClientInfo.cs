6b0d966ad3bc3c01dd017628a14414ccc8e42fcf
Spencer James <spencer.ws.james@me.com>
2026-08-11 19:20:54 -0700

    added MultiTorHttpClientInfo.DoesNotExistForUser

--- src/Kokoabim.OnionHttpClient/Models/MultiTorHttpClientInfo.cs
+++ src/Kokoabim.OnionHttpClient/Models/MultiTorHttpClientInfo.cs
@@ -4,6 +4,7 @@ public class MultiTorHttpClientInfo
 {
     public Exception? Error { get; set; }
     public int ClientCount { get; set; }
+    public bool DoesNotExistForUser { get; set; }
     public int RequestCount { get; set; }
     public TorHttpClientStatus Status { get; set; }
     public TorHttpClientInfo[] TorHttpClientsInfo { get; set; } = [];
